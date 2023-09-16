289a16830f9729c5e6038b29810faf6618f2f975
shawonTalukder <shawontalukder.study@gmail.com>
2023-09-16 17:00:59 +0600

    accepts element for styling in paragraph

--- app/components/ui/Tooltip.tsx
+++ app/components/ui/Tooltip.tsx
@@ -1,13 +1,14 @@
 "use client";
 
+import { ReactElement } from "react";
 import { IconType } from "react-icons";
 
 interface TooltipProps {
   icon: IconType;
-  label: string;
+  label: ReactElement<any, any>;
 }
 
-const Tooltip: React.FC<TooltipProps> = ({ icon: Icon, label }) => {
+const Tooltip: React.FC<TooltipProps> = ({ icon: Icon, label: Label }) => {
   return (
     <div className="group relative hidden md:block">
       <Icon
@@ -15,7 +16,7 @@ const Tooltip: React.FC<TooltipProps> = ({ icon: Icon, label }) => {
         className="text-neutral-600 cursor-pointer hover:text-neutral-200"
       />
       <span className="absolute hidden group-hover:block min-w-[300px] max-w-[400px] border-2 border-indigo-500 rounded-lg bg-neutral-800 text-neutral-200 px-4 py-4 top-3 left-2 text-sm text-center">
-        {label}
+        {Label}
       </span>
     </div>
   );
