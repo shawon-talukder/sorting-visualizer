a1ca0844dc658c608ca88fa2c6950b5710341a84
shawonTalukder <shawontalukder.study@gmail.com>
2023-09-16 16:51:08 +0600

    moved to ui component. [NEW] tooltip for ui

---
+++ app/components/ui/Button.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+interface ButtonProps {
+  label: string;
+  onAction: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  disabled?: boolean;
+  isSecondary?: boolean;
+  small?: boolean;
+  outline?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  label,
+  onAction,
+  disabled,
+  isSecondary,
+  outline,
+  small,
+}) => {
+  return (
+    <button
+      className={`relative w-full font-semibold rounded-md transform transition
+      hover:opacity-80 active:translate-y-[1px]  
+      disabled:opacity-70 disabled:cursor-not-allowed 
+      ${outline ? "border-2 border-indigo-700 " : ""}
+       ${small ? "px-2" : "px-4"}
+      ${small ? "py-1" : "py-2"}
+      ${isSecondary ? "bg-white" : "bg-indigo-300"}`}
+      onSubmit={onAction}
+      disabled={disabled}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Button;
---
+++ app/components/ui/Tooltip.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { IconType } from "react-icons";
+
+interface TooltipProps {
+  icon: IconType;
+  label: string;
+}
+
+const Tooltip: React.FC<TooltipProps> = ({ icon: Icon, label }) => {
+  return (
+    <div className="group relative hidden md:block">
+      <Icon
+        size={16}
+        className="text-neutral-600 cursor-pointer hover:text-neutral-200"
+      />
+      <span className="absolute hidden group-hover:block min-w-[300px] max-w-[400px] border-2 border-indigo-500 rounded-lg bg-neutral-800 text-neutral-200 px-4 py-4 top-3 left-2 text-sm text-center">
+        {label}
+      </span>
+    </div>
+  );
+};
+
+export default Tooltip;
