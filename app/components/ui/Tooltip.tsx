7da391544081b6a1a0e7a010fbb416d8295ee333
shawonTalukder <shawontalukder.study@gmail.com>
2023-09-16 17:05:27 +0600

    changes in style

--- app/components/ui/Tooltip.tsx
+++ app/components/ui/Tooltip.tsx
@@ -15,7 +15,7 @@ const Tooltip: React.FC<TooltipProps> = ({ icon: Icon, label: Label }) => {
         size={16}
         className="text-neutral-600 cursor-pointer hover:text-neutral-200"
       />
-      <span className="absolute hidden group-hover:block min-w-[300px] max-w-[400px] border-2 border-indigo-500 rounded-lg bg-neutral-800 text-neutral-200 px-4 py-4 top-3 left-2 text-sm text-center">
+      <span className="absolute hidden group-hover:block min-w-[300px] max-w-[400px] border-2 border-indigo-500 rounded-lg bg-neutral-800 text-neutral-200 px-4 py-4 top-3 left-2 text-center">
         {Label}
       </span>
     </div>
