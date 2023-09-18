2919004f6b2c28df2f2407bc32de195a77cd5a85
shawonTalukder <shawontalukder.study@gmail.com>
2023-09-18 18:09:36 +0600

    add suspense.

--- app/components/Visualizer.tsx
+++ app/components/Visualizer.tsx
@@ -1,7 +1,21 @@
+"use client";
+
+import { Suspense } from "react";
+
+import Loading from "../loading";
+import ArrayBars from "./ArrayBars";
 import Container from "./Container";
 
 const Visualizer = () => {
-  return <Container>hello</Container>;
+  return (
+    <Container>
+      <div className="flex flex-col lg:flex-row gap-3 lg:gap-2 justify-between items-center mt-2 lg:h-full ">
+        <Suspense fallback={<Loading />} >
+          <ArrayBars />
+        </Suspense>
+      </div>
+    </Container>
+  );
 };
 
 export default Visualizer;
