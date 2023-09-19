7b31dfbc641fafe50432bb62b3cd6797ef654ed9
shawonTalukder <shawontalukder.study@gmail.com>
2023-09-20 01:47:54 +0600

    add id to the container, to remove and append child

--- app/components/ArrayBars.tsx
+++ app/components/ArrayBars.tsx
@@ -15,7 +15,7 @@ const ArrayBars = () => {
   }
 
   return (
-    <div className="flex flex-1 gap-1 justify-center items-end bg-slate-950 rounded-lg px-2 lg:px-4 py-4 lg:py-8 border border-cyan-300 h-[620px] w-full">
+    <div id="divContainer" className="flex flex-1 gap-1 justify-center items-end bg-slate-950 rounded-lg px-2 lg:px-4 py-4 lg:py-8 border border-cyan-300 h-[620px] w-full">
       {array.map((el, ind) => (
         <div
           key={ind}
