528e2483530680322e5cb258224209991ef6fa78
shawonTalukder <shawontalukder.study@gmail.com>
2023-09-20 18:22:05 +0600

    utility function to check if two arrays are equal or not

--- app/utils/array.ts
+++ app/utils/array.ts
@@ -20,3 +20,27 @@ export const generateArray = (arrLength: number) => {
 
   return array;
 };
+
+/***************************************************************************
+ *
+ *
+ ->Description: check if two arrays are equal or not
+ ----->@Params: array1: @Type: array of numbers
+                array2: @Type: array of numbers
+ ---->@returns: boolean, True if arrays are equal else false
+ *
+ *
+ ***************************************************************************
+ */
+export const areArraysEqual = (array1: number[], array2: number[]) => {
+  let n = array1.length;
+  let m = array2.length;
+
+  if (n !== m) return false;
+
+  for (let i = 0; i < n; i++) {
+    if (array1[i] !== array2[i]) return false;
+  }
+
+  return true;
+};
