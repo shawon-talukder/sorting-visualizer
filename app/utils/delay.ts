17fc9cc8b1ad245a7a359c36bb0f292436f23c82
shawonTalukder <shawontalukder.study@gmail.com>
2023-09-20 16:41:33 +0600

    return delay ms for animation

--- app/utils/delay.ts
+++ app/utils/delay.ts
@@ -12,3 +12,15 @@
 export const delay = async (ms: number) => {
   return new Promise((resolve) => setTimeout(resolve, ms));
 };
+
+export const getDelayInMS = (arrayLength: number) => {
+  return arrayLength > 200
+    ? 5
+    : arrayLength > 80
+    ? 30
+    : arrayLength > 40
+    ? 100
+    : arrayLength > 20
+    ? 150
+    : 400;
+};
