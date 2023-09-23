74b6fc16f88a9721681386f2515fabd1d7b08664
shawonTalukder <shawontalukder.study@gmail.com>
2023-09-24 01:00:13 +0600

    feat: Bubble sort animation added from algorithm

--- app/algorithms/BubbleSort.ts
+++ app/algorithms/BubbleSort.ts
@@ -9,8 +9,11 @@
  ***********************************************************************************************
  */
 
-export function BubbleSort(arr: number[]) {
+import { AnimationTypes } from "../types";
+
+export function BubbleSort(arr: number[]): AnimationTypes[] {
   let n = arr.length;
+  const animations: AnimationTypes[] = [];
 
   for (let i = 0; i < n - 1; i++) {
     // declaring swap variable to optimize the sorting algorithm
@@ -18,15 +21,24 @@ export function BubbleSort(arr: number[]) {
     let swapped = false;
 
     for (let j = 0; j < n - i - 1; j++) {
+      const animation: AnimationTypes = { swap: [], comparison: [] };
+
+      animation.comparison.push(j, j + 1);
+
       if (arr[j] > arr[j + 1]) {
+        animation.swap.push(arr[j], arr[j + 1]);
+
         // swap both array element
         [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]];
         swapped = true;
       }
+      animations.push(animation);
     }
 
     // optimizing
     // happens only when the array is fully sorted
     if (!swapped) break;
   }
+
+  return animations;
 }
