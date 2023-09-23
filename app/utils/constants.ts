ccc150502c837d99e233651f4bd58d45dcd7833b
shawonTalukder <shawontalukder.study@gmail.com>
2023-09-23 18:01:01 +0600

    update: min index color change

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -12,4 +12,4 @@
 export const COMPARISON_COLOR = "bg-violet-600"; // #a78bfa
 export const DIFF_COLOR = "bg-rose-600"; // #e11d48
 export const CURRENT_COLOR = "bg-amber-800"; // #92400e
-export const MIN_COLOR = "bg-orange-800"; // #9a3412
+export const MIN_COLOR = "bg-green-400"; // #9a3412
