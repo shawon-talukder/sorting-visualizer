afe578ec91ec49fb0a4fe8ce6874e6462fb4e776
shawonTalukder <shawontalukder.study@gmail.com>
2023-09-23 17:12:46 +0600

    chore: one single index file to export all from here

---
+++ app/components/animations/index.ts
@@ -0,0 +1,5 @@
+import { mergeSortAnimation } from "./MergeSortAnimation";
+import { selectionSortAnimation } from "./SelectionSortAnimation";
+
+// export
+export { mergeSortAnimation, selectionSortAnimation };
