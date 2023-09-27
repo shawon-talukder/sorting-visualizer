04b02d045e0d052c2d01292da31ccb85dfd09c45
shawonTalukder <shawontalukder.study@gmail.com>
2023-09-27 10:51:18 +0600

    chore: dynamically import arraybars

--- app/components/Visualizer.tsx
+++ app/components/Visualizer.tsx
@@ -1,16 +1,19 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import { Suspense } from "react";
 
 import Loading from "../loading";
-import ArrayBars from "./ArrayBars";
 import Container from "./Container";
 
+const ArrayBars = dynamic(() => import("./ArrayBars"), {
+  loading: () => <Loading />,
+});
 const Visualizer = () => {
   return (
     <Container>
       <div className="flex flex-col lg:flex-row gap-3 lg:gap-2 justify-between items-center mt-2 lg:h-full ">
-        <Suspense fallback={<Loading />} >
+        <Suspense fallback={<Loading />}>
           <ArrayBars />
         </Suspense>
       </div>
